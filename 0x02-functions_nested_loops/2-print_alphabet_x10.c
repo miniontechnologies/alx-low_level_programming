c6f1ca28e7d08c05d315c28853b38c066c58ea88
Miniontechnologies <Kiptookigendennis@google.com>
2022-10-19 19:45:20 -0700

    all

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,18 @@
 
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ *
  */
 
 void print_alphabet_x10(void)
 {
-	init count = 0;
+	int ten;
 	char letter;
 
-	while (count++ <= 9)
+	for (ten = 0; ten <= 9; ten++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++
+		for (letter = 'a'; letter <= 'z'; letter++)
 				_putchar(letter);
 		_putchar('/n');
 	}
+}
