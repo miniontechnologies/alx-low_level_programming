f04790ce7d79d31e229472473364a0cbc4f46d5a
Miniontechnologies <Kiptookigendennis@google.com>
2022-10-19 19:53:34 -0700

    all

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  * Return: 1 if char is letter lower or upper, 0 otherwise
  */
 
-int _isalpha(init c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
