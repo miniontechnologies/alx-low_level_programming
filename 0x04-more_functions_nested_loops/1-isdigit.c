0db88349940472c3a3fff2f20c7e7134b4475106
Miniontechnologies <Kiptookigendennis@google.com>
2022-10-21 05:59:01 -0700

    all

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if the no is n/n 0 to 9
+ * @c: input no
+ * Return: 1 if it is a number (0 to 9), 0 in other case
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
