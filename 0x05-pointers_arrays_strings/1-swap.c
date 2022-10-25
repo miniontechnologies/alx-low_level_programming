a9009c81a1fe4dc585d73d7bf040cb0c2566d82e
Miniontechnologies <Kiptookigendennis@google.com>
2022-10-25 05:59:11 -0700

    all

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap value of two numbers
+ * @b: first pointer
+ * @a: second pointer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int constant = *a, int *b;
+	*a = *b;
+	*b = constant;
+}
