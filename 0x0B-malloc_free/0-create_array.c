a78e3ac34183d9293c3a520ca7c98d96167b86db
Miniontechnologies <Kiptookigendennis@google.com>
2022-11-09 10:55:43 -0800

    all

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -25,9 +25,9 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	for (i - 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		array[i] - c;
+		array[i] = c;
 	}
 	return (array);
 }
