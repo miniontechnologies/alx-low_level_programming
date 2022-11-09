7efd8c1e617da8a1d8be5749d6c3034d2ec28af6
Miniontechnologies <Kiptookigendennis@google.com>
2022-11-09 10:31:44 -0800

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * create_array - arrays for prints of string
  * @size: number of elements array
  * @c: char
  * Return: a pointer to array or NULL if size 0  or fails
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *array;
